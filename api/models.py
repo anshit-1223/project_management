4d6eee192b6eda209de46dc6f5a997663c441fe7
anshit-1223 <81354203+anshit-1223@users.noreply.github.com>
2024-12-31 14:09:34 +0530

    Add Project and Memebrs in models

--- api/models.py
+++ api/models.py
@@ -13,4 +13,17 @@ class Users(models.Model):
     date_joined=models.DateTimeField(auto_now=True)
 
 
-    
+#Projects Details
+class Projects_Details(models.Model):
+    project_id=models.AutoField(primary_key=True)
+    project_name=models.CharField(max_length=100)
+    project_description=models.TextField()
+    project_owner=models.ForeignKey(Users,on_delete=models.CASCADE)
+    project_created_at=models.DateTimeField(auto_now=True)
+
+#Project Members
+class Project_Members(models.Model):
+    prmember_id=models.AutoField(primary_key=True)
+    prmember_project=models.ForeignKey(Projects_Details,on_delete=models.CASCADE)
+    prmember_user=models.ForeignKey(Users,on_delete=models.CASCADE)
+    prmember_role=models.CharField(max_length=50,choices=(('Admin','Admin'),('Member','Member')))
